47aae97b417b95f2784b80fe5031d6f920db7ef7
Leon Ho <leon@vinova.sg>
2015-05-08 00:32:47 +0700

    example for challenge game

--- server/socket/test.js
+++ server/socket/test.js
@@ -3,13 +3,29 @@ module.exports = function(io){
 	io.on('connection', function(socket){
 	  console.log('a user connected');
 
-	  socket.on('chat message', function(msg){
-	    console.log('message: ' + msg);
-	    if(msg == 'fuck') io.emit('chat message', 'cam noi bay');
+	  socket.on('challenge-game', function(info){
+	  	if(info.room && info.user){
+	  		socket.username = info.user;
+	  		socket.room = info.room;
+	  		socket.join(info.room);
+	  		console.log("%s accepted challenge, room: %s", info.user, info.room);
+	  	}
+	  });
+
+	  socket.on('update-status-server', function(status){
+	  	if(status.room && status.data){
+	  		console.log(status.room + "-" + status.data);
+	  		socket.broadcast.to(status.room).emit('update-status-client', {data: status.data});
+	  	}
 	  });
 
 	  socket.on('disconnect', function(){
-	    console.log('user disconnected');
+	  	if(socket.room){
+	  		console.log('user %s disconnected, room: %s', socket.username, socket.room);
+	    	socket.leave(socket.room);
+	  	}else{
+	  		console.log('user disconnected');
+	  	}
 	  });
 	});
 }
